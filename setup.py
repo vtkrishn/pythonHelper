ec708f3c17550985af0981dc82bbb84f99b00aa9
Vinod Krishnan <vin.sinin@gmail.com>
2017-09-25 22:18:01 -0700

    travis

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 	'Intended Audience :: Developers',
 	'Topic :: Software Development :: Build Tools',
 	'License :: OSI Approved :: MIT License',
-	'Programming Language :: Python :: 3.4'
+	'Programming Language :: Python :: 2.7'
 	],
 	entry_points={
     'console_scripts': [
---
+++ tests.py
@@ -0,0 +1,3 @@
+from pyModule import helloModule
+
+helloModule.hello()
